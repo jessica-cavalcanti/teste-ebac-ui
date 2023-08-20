9a605438e60c87a4eca4ce863271ad0f7bbdafa7
Jessica <paula.jessica104@gmail.com>
2023-08-19 23:20:30 -0300

    Usando massa de dados em lista

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -13,8 +15,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
  
    
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+       EnderecoPage.editarEnderecoFaturamento('Jéssica', 'de Paula', 'Google', 'Brasil', 'Rua Maria do Carmo', '212', 'São Paulo', 'São Paulo', '08121000', '11948930039', 'teste@dominio.br') 
+       cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+ 
+     });
 });
